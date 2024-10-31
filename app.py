7998ab86d29376a17ad4353ab49caccfaf66d6f7
RileyGatto <rmgatto64@gmail.com>
2024-10-31 17:53:41 +1100

    The UI has been styled and an asset folder has been used to get rid of body margin

--- app.py
+++ app.py
@@ -1,45 +1,89 @@
-from dash import Dash, dcc, html
-import plotly.express as px
 import pandas as pd
+from dash import Dash, html, dcc, Input, Output, callback
+import plotly.express as px
+
+data = pd.read_csv('complete_sales_data.csv')
 
 app = Dash(__name__)
 
+
 colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+    'main' : '#1f2630',
+    'secondary' : '#252e3f',
+    'text': '#fefefe',
+    'secondText': "#2cfec1"
 }
 
-df = pd.read_csv("complete_sales_data.csv")
+
+#Graph
+def gen_fig(data):
+    fig = px.line(data, x="date", y="sales")
+    fig.update_layout(
+        plot_bgcolor=colors['main'],
+        paper_bgcolor=colors['secondary'],
+        font_color=colors['secondText']
+    )
+    fig.update_traces(line=dict(color=colors['secondText']))  # dict = dictionary
+    return fig;
 
 
-fig = px.line(df, x = "date", y = "sales", title="Pink Morsel Sales")
-
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
+visulation = dcc.Graph(
+    id="visualization",
+    figure=gen_fig(data),
+    
 )
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
+#Header
+header = html.H1(
+    "Pink Morsel Sales",
+    id="header",
+    style={
+        'textAlign' : 'center',
+        'color': colors['secondText'],
+        'margin': '40px 0',
+        
+    }
+)
 
-    html.Div(children='Dash: A web application framework for your data.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
+region_picker = dcc.RadioItems(
+    ["all","north","east","south","west"],"all",
+    id="region_picker",
+    inline=True,
+    style={
+        'color' : colors['secondText'],
+        'backgroundColor': colors['secondary'],
+        'padding': '0 0 40px 0'
+    }
+)
 
-    dcc.Graph(
-        id='pink morsel sales',
-        figure= fig
-    )
-])
+@app.callback (
+    Output('visualization', "figure"),
+    Input('region_picker', "value")
+)
+def update_graph(region):
+    if region == 'all':
+        trimmed_data = data;
+    else:
+        trimmed_data = data[data["region"] == region]
+    
+    fig=gen_fig(trimmed_data)
+    return fig
+
+app.layout = html.Div(
+    [
+        header,
+        visulation,
+        region_picker
+    ],
+    style={
+        "textAlign": "center",
+        "background-color": colors["main"],
+        'height': '100vh',
+    }
+)
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run_server()
+    app.run(debug=True)
+    #app.run_server()
+
+
